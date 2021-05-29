8023f8b9e4e52e5543eafef0c556db1d6f171fcb
Shakil Ahmmed <shakilfci461@gmail.com>
2021-05-30 01:59:37 +0600

    Sequilize

--- config/application.js
+++ config/application.js
@@ -1,3 +1,4 @@
+
 const config = {
     database: {
         DB_CONNECTION: "mysql",
--- src/models/user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('User', {
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        email: DataTypes.STRING,
-    }, {});
-};
